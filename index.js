424326f7e73154f7e7c8aff229346668cec53462
aleksandr.belets <aleksandr.belets@solomoto.com>
2017-07-19 14:37:26 +0300

    fixes for lint

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
-import { NativeModules } from 'react-native';
+import { NativeModules } from 'react-native'
 
 export const Scopes = {
-  VALUABLE_ACCESS: 'VALUABLE_ACCESS',
-  SET_STATUS: 'SET_STATUS',
-  PHOTO_CONTENT: 'PHOTO_CONTENT',
-  GROUP_CONTENT: 'GROUP_CONTENT',
-  VIDEO_CONTENT: 'VIDEO_CONTENT',
-  APP_INVITE: 'APP_INVITE',
-  LONG_ACCESS_TOKEN: 'LONG_ACCESS_TOKEN'
+    VALUABLE_ACCESS: 'VALUABLE_ACCESS',
+    SET_STATUS: 'SET_STATUS',
+    PHOTO_CONTENT: 'PHOTO_CONTENT',
+    GROUP_CONTENT: 'GROUP_CONTENT',
+    VIDEO_CONTENT: 'VIDEO_CONTENT',
+    APP_INVITE: 'APP_INVITE',
+    LONG_ACCESS_TOKEN: 'LONG_ACCESS_TOKEN'
 }
 
-export default NativeModules.OkManager;
+export default NativeModules.OkManager
